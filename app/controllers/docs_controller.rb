13ec4abe9700fa464d94263e3e363ebe87aec934
Jesse L Lawson <homestarflyer@gmail.com>
2016-09-18 15:59:22 -0400

    doc_params

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,9 +1,13 @@
 class DocsController < ApplicationController
-
+  before_action :find_doc, only: [:show, :edit, :update, :destroy]
+  
   def index
+    @doc = Doc.all
   end
+
   def show
   end
+
   def new
     @doc = Doc.new
   end
@@ -25,6 +29,7 @@ class DocsController < ApplicationController
   end
    private
    def find_doc
+     @doc = Doc.find(params[:id])
    end
    def doc_params
      params.require(:doc).permit(:title, :content)
