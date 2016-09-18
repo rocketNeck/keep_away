8040927bdbf7d6b4ac336cfae38ab479a7863f53
Jesse L Lawson <homestarflyer@gmail.com>
2016-09-18 16:16:19 -0400

    Edie and destroy docs

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -23,10 +23,20 @@ class DocsController < ApplicationController
 
   def edit
   end
+
   def update
+    if @doc.update(doc_params)
+      redirect_to @doc
+    else
+      render 'edit'
+    end
   end
+
   def destroy
+    @doc.destroy
+    redirect_to docs_path
   end
+
    private
    def find_doc
      @doc = Doc.find(params[:id])
